e1094d3aca6fa25fc1f560af0f387b5146fda9ef
unknown <firas.jerbi.engineer@gmail.com>
2025-06-08 12:32:04 +0200

    backend challenge CRUD

--- backend/src/database/db.py
+++ backend/src/database/db.py
@@ -2,10 +2,12 @@ from sqlalchemy import Session
 from datetime import datetime, timedelta
 from . import models
 
+
 def get_challenge_quota(db: Session, user_id:str):
     return (db.query(models.ChallengeQuota)
             .filter(models.ChallengeQuota.user_id == user_id)
             .first())
+
 
 def create_challenge_quota(db: Session, user_id:str):
     db_quota = models.ChallengeQuota(user_id=user_id)
@@ -13,3 +15,38 @@ def create_challenge_quota(db: Session, user_id:str):
     db.commit()
     db.refresh(db_quota)
     return db_quota
+
+
+def reset_quota_if_needed(db: Session, quota: models.ChallengeQuota):
+    now = datetime.now()
+    if now - quota.last_reset_date > timedelta(hours=24):
+        quota.remaining_quota = 10
+        quota.last_reset_date = now
+        db.commit()
+        db.refresh(quota)
+    return quota
+
+
+def create_challenge(db: Session,
+                     dofficulty: str,
+                     created_by: str,
+                     title: str,
+                     options: str,
+                     corret_answer_id: int,
+                     explanation: str):
+    db_challenge = models.Challenge(difficulty=dofficulty,
+                                    created_by=created_by,
+                                    title=title,
+                                    options=options,
+                                    correct_answer_id=corret_answer_id,
+                                    explanation=explanation)
+    db.add(db_challenge)
+    db.commit()
+    db.refresh(db_challenge)
+    return db_challenge
+
+
+def get_user_challenges(db: Session, user_id: str):
+    return (db.query(models.Challenge)
+            .filter(models.Challenge.created_by == user_id)
+            .all())
